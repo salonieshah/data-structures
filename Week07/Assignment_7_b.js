a0308bca5608f5f8a323dc91e393ae31f15a78b2
salonieshah <shahs975@newschool.edu>
2019-10-18 23:39:56 +0000

    update

--- Week07/Assignment_7_b.js
+++ Week07/Assignment_7_b.js
@@ -9,48 +9,52 @@ var async = require('async'); // npm install async
 var fs = require('fs');
 const dotenv = require('dotenv'); // npm install dotenv
 
-// TAMU api key
+// 2. Get TAMU api key
 dotenv.config();
 const apiKey = process.env.TAMU_Key;
 
 
-// read json address file 
-var content = fs.readFileSync('aaData.json');
+// 3. Read the JSON file and parse the contents of the file.
+var content = fs.readFileSync('aa.json');
 content = JSON.parse(content);
-// console.log(content);
-// console.log(content.length);
+console.log(content);
+console.log(content.length);
 
-
-
+// 5. Created an empty array 
 var meetingsData = [];
 
+// 6. Use .eachSeries in the async module to iterate over an array and operate on each item in the array in series
 async.eachSeries(content, function(value, callback) {
+    var address = value.locationDetails.streetAddress;
     var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
-    apiRequest += 'streetAddress=' + value.locationDetails.streetAddress.split(' ').join('%20');
+    apiRequest += 'streetAddress=' + address.split(' ').join('%20');
     apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
     apiRequest += '&zip=' + value.zipCode;
     apiRequest += '&format=json&version=4.01';
+    
+// 7. Requesting the contents in the body of the link by using the request. 
     
     request(apiRequest, function(err, resp, body) {
         if (err) {throw err;}
         else {
             var tamuGeo = JSON.parse(body);
-            
+
+// 8. Create a variable containing an object which contains the required information from the body (Street Address, Longitude, Latitude)             
             var geoLocation = {};
             
-            geoLocation.address = tamuGeo['InputAddress']['StreetAddress'];
+            geoLocation.address = tamuGeo['InputAddress'];
             geoLocation.latitude = tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude'];
             geoLocation.longitude = tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Longitude'];
-        
-            value.geoLocation = geoLocation;
-            
-            meetingsData.push(value);
+
+// 9. Pushing the object in the empty array that we created at the begining of the file          
+            meetingsData.push(geoLocation);
         }
     
     });
     
     setTimeout(callback, 2000);
-
+    
+// 10. Write a .json file containing the values of the array.  
 }, function() {
     fs.writeFileSync('Assignment_7_b.json', JSON.stringify(meetingsData));
     console.log(meetingsData.length);
