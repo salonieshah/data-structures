f09f4017c826e2cb497ab1af9ddd9d0b57ccd0f0
salonieshah <shahs975@newschool.edu>
2019-10-22 22:32:50 +0000

    update

---
+++ Week07/Assignment_7_f.js
@@ -0,0 +1,36 @@
+// Saloni Shah
+// Data Structure: Assignment 06 
+// Date: 09 October 2019
+
+//Lets Get Started
+// 1. Create dependencies and configure
+const { Client } = require('pg');
+const cTable = require('console.table');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//2. AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'salonieshah';
+db_credentials.host = 'data-structures.c0wrpagx3mr8.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+//3. Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+// console.log(client);
+
+//4. Sample SQL statement to query meetings at 619 LEXINGTON AVE New York NY 
+var thisQuery = `SELECT * FROM aaData WHERE Street_Address = '273 Bowery Street'`;
+//var thisQuery = "SELECT mtgday, mtgtime, mtglocation, mtgaddress, mtgtypes FROM aadata WHERE mtgday = 'Monday' and mtghour >= 7;";
+
+client.query(thisQuery, (err, res) => {
+    if (err) {throw err}
+    else {
+        console.log('query successful');
+        console.table(res.rows);
+        client.end();
+    }
+});
